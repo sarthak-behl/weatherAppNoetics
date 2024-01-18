d00c8c8e6004c311c5eef15b0a352493f242096b
SARTHAK BEHL <89195869+sarthak-behl@users.noreply.github.com>
2024-01-18 12:57:00 +0530

    Update WeatherService.java
    commit ser

--- weather/src/main/java/com/weatherApi/weather/WeatherService.java
+++ weather/src/main/java/com/weatherApi/weather/WeatherService.java
@@ -7,8 +7,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class WeatherService {
@@ -19,7 +24,7 @@ public class WeatherService {
     @Value("${openweathermap.apiKey}")
     private String apiKey;
 
-    public WeatherData getWeather(String cityName) throws JsonProcessingException {
+    public WeatherData getWeather(String cityName) throws JsonProcessingException, WeatherNotFoundException {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
         RestTemplate restTemplate = new RestTemplate();
@@ -28,10 +33,13 @@ public class WeatherService {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(result);
 
+        if (jsonNode.has("cod") && jsonNode.get("cod").asInt() == 404) {
+            // Handle city not found
+            throw new WeatherNotFoundException("Weather information not found for city: " + cityName);
+        }
+
         WeatherData weatherData = new WeatherData();
         weatherData.setCityName(cityName);
-        //weatherData.setWeatherResponse(result);
-
         weatherData.setTemp(jsonNode.path("main").path("temp").asDouble());
         weatherData.setFeelsLike(jsonNode.path("main").path("feels_like").asDouble());
         weatherData.setPressure(jsonNode.path("main").path("pressure").asInt());
@@ -39,8 +47,34 @@ public class WeatherService {
         weatherData.setLon(jsonNode.path("coord").path("lon").asDouble());
         weatherData.setLat(jsonNode.path("coord").path("lat").asDouble());
 
+
         weatherDataRepository.save(weatherData);
 
         return weatherData;
     }
+
+
+
+    public Page<WeatherData> getAllWeather(int page, int pageSize) throws JsonProcessingException, WeatherNotFoundException {
+        PageRequest pageRequest = PageRequest.of(page, pageSize);
+        return weatherDataRepository.findAll(pageRequest);
+    }
+
+
+
+
+    public List<WeatherData> sortBasedUponSomeField(String field) throws WeatherNotFoundException {
+        List<WeatherData> sortedWeatherData = weatherDataRepository.findAll(Sort.by(Sort.Direction.ASC, field));
+
+        if (sortedWeatherData.isEmpty()) {
+            throw new WeatherNotFoundException("Weather information not found for any city.");
+        }
+
+        return sortedWeatherData;
+    }
+
+
+
+
+
 }
